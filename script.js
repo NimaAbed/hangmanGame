7118f4be60468a07465797bd1ce61ffeaac27443
Nima Abed <abodnima84@gmail.com>
2023-06-20 10:26:13 +0330

    Create Func Random Word

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const secretWords = ["break","apple","moon","rice","day","night"]
+
+let randomWord=""
+
+function selectRandomWord(){
+    randomWord=secretWords[Math.floor(Math.random()*secretWords.length)]
+    console.log(randomWord)
+}
+
+
+
+selectRandomWord()
