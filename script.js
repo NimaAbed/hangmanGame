a1021ad7c280248369912fe82f4a71cf0e05a4b2
Nima Abed <abodnima84@gmail.com>
2023-06-20 10:28:22 +0330

    Add Event Listener #letters

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let randomWord=""
 function selectRandomWord(){
     randomWord=secretWords[Math.floor(Math.random()*secretWords.length)]
     console.log(randomWord)
+    document.querySelector("#letters").addEventListener("click",btnHandler)
+}
+
+function btnHandler(event){
+    console.log(event.target)
+    
 }
 
 
